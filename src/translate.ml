8b39b1825a7bba1ad4f3219903cba18bef947620
Zach Cheslock <zcc9@cornell.edu>
2024-06-11 11:39:51 -0400

    Added translate function skeletons

--- src/translate.ml
+++ src/translate.ml
@@ -1,6 +1,8 @@
+open Support.FileInfo
+
 type symbol = string
 
-type fpcore = FPCore of (symbol * argument list * property list * expr)
+type fpcore = FPCore of (symbol * argument list * property list * expr) | TEST
 and dimension = Num of int | DSymbol of symbol
 and argument = ASymbol of symbol | Array of (symbol * dimension list)
 
@@ -16,3 +18,13 @@ and op = Plus | Times | Divide | Sqrt | Equals | LessThan
 and property
 
 type program = fpcore list
+
+let get_name (inf : info) = match inf with FI (sym, _, _) -> sym | UNKNOWN -> ""
+
+let rec translate (prog : Syntax.term) : program = match prog with 
+| TmAbs (inf, bind, typ, func) -> [(translate_function "" (bind.b_name) typ func)]
+| TmLet (inf, bind, typ, t1, t2) -> [(translate_function (get_name inf) (bind.b_name) typ t1)] @ translate t2
+| _ -> [TEST]
+
+
+and translate_function (name : symbol) (arg : string) (typ : Syntax.ty) (exp : Syntax.term) : fpcore = TEST
