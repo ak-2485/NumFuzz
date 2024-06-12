fcfa1e5713ceb95dbe4f87c9cc8a97ace70280ca
Zach Cheslock <zcc9@cornell.edu>
2024-06-12 10:53:21 -0400

    Wrote overall translate function with control flow to helper functions

--- src/translate.ml
+++ src/translate.ml
@@ -1,8 +1,9 @@
 open Support.FileInfo
+open Syntax
 
 type symbol = string
 
-type fpcore = FPCore of (symbol * argument list * property list * expr) | TEST
+type fpcore = FPCore of (symbol * argument list * property list * expr)
 and dimension = Num of int | DSymbol of symbol
 and argument = ASymbol of symbol | Array of (symbol * dimension list)
 
@@ -19,12 +20,29 @@ and property
 
 type program = fpcore list
 
-let get_name (inf : info) = match inf with FI (sym, _, _) -> sym | UNKNOWN -> ""
+let get_name (inf : info) =
+  match inf with FI (sym, _, _) -> sym | UNKNOWN -> ""
 
-let rec translate (prog : Syntax.term) : program = match prog with 
-| TmAbs (inf, bind, typ, func) -> [(translate_function "" (bind.b_name) typ func)]
-| TmLet (inf, bind, typ, t1, t2) -> [(translate_function (get_name inf) (bind.b_name) typ t1)] @ translate t2
-| _ -> [TEST]
+let rec translate (prog : term) : program =
+  match prog with
+  | TmAbs (inf, bind, typ, t) ->
+      let arg_list, body = get_arguments prog in
+      [ FPCore ("", arg_list, [], translate_body body) ]
+  | TmLet (inf, bind, typ, t1, t2) -> (
+      match t1 with
+      | TmAbs _ ->
+          let arg_list, body = get_arguments prog in
+          FPCore ("", arg_list, [], translate_body body) :: translate t2
+      | _ ->
+          failwith
+            "FPCore does not support expressions outside of function bodies"
+          (* ([ translate_function (get_name inf) bind.b_name typ t1 ] @ translate t2) *)
+      )
+  | _ ->
+      failwith "FPCore does not support expressions outside of function bodies"
 
+(* Assumes that [prog] has outermost TmAbs *)
+and get_arguments (prog : term) : argument list * term =
+  ([], TmPrim (UNKNOWN, PrimTUnit))
 
-and translate_function (name : symbol) (arg : string) (typ : Syntax.ty) (exp : Syntax.term) : fpcore = TEST
+and translate_body (body : term) : expr = ENum 0.0
