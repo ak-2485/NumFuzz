7212ea040771cc652271618b2fc8f4dfbf48315c
ak2485 <ak2485@cornell.edu>
2024-11-13 12:39:48 -0500

    Remove FMA in horner

--- examples/Gappa/gen_horner.py
+++ examples/Gappa/gen_horner.py
@@ -10,9 +10,9 @@ def generate_horner_script(order):
     # Generate the rounded computation
     for i in range(order - 1, -1, -1):
         if i == order - 1:
-            script += f"z{i}  = rnd(a{order} * x + a{i});\n"
+            script += f"z{i}  rnd= (a{order} * x + a{i});\n"
         else:
-            script += f"z{i}  = rnd(z{i+1} * x + a{i});\n"
+            script += f"z{i}  rnd= (z{i+1} * x + a{i});\n"
     # Add the logical formula for Gappa to prove
     script += "\n# the logical formula that Gappa will try (and succeed) to prove\n"
     script += "{ x in [0.1,1000]"
