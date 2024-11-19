1d9bbd26a6e190bbb9cce5413fc15f9daa6c3e40
Ariel Kellison <ak2485@cornell.edu>
2024-11-19 10:24:42 -0500

    Update horner gen for rounding

--- examples/Gappa/gen_horner.py
+++ examples/Gappa/gen_horner.py
@@ -10,9 +10,9 @@ def generate_horner_script(order):
     # Generate the rounded computation
     for i in range(order - 1, -1, -1):
         if i == order - 1:
-            script += f"z{i}  rnd= (a{order} * x + a{i});\n"
+            script += f"z{i}  = rnd(a{order} * x + a{i});\n"
         else:
-            script += f"z{i}  rnd= (z{i+1} * x + a{i});\n"
+            script += f"z{i}  = rnd(z{i+1} * x + a{i});\n"
     # Add the logical formula for Gappa to prove
     script += "\n# the logical formula that Gappa will try (and succeed) to prove\n"
     script += "{ x in [0.1,1000]"
@@ -21,6 +21,6 @@ def generate_horner_script(order):
     script += " -> |(z0 - r0) / r0| in ? }"
     return script
 # Generate the script for a 500th-order polynomial
-order = 500
+order = 200
 with open(f"Horner{order}.g","w") as f:
     f.write(f"{generate_horner_script(order)}")
